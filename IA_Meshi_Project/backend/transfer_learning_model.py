67d82eddc6a7f2d14c9984d7513095dc98705fef
JeanAlexandreVergaraUSM <jean.alexandre@usm.cl>
2024-10-24 18:32:57 -0300

    Update and rename transfer_learning_model.py to transfer_learning_model.py

--- IA_Meshi_Project/backend/transfer_learning_model.py
+++ IA_Meshi_Project/backend/transfer_learning_model.py
@@ -4,7 +4,6 @@ from torchvision import models, transforms
 from PIL import Image
 import os
 
-# Transformations for the image
 data_transforms = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
@@ -12,10 +11,8 @@ data_transforms = transforms.Compose([
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
 
-# Obtener la ruta base (IA_Meshi_Project)
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Función para cargar cada modelo entrenado con su número de clases correspondiente
 def load_model(model_path, num_classes):
     model = models.resnet18(pretrained=False)
     num_ftrs = model.fc.in_features
@@ -24,19 +21,16 @@ def load_model(model_path, num_classes):
     model.eval()  # Poner el modelo en modo de evaluación
     return model
 
-# Cargar los modelos con sus archivos .pth correspondientes
 models = [
     load_model(os.path.join(base_dir, 'Entreno', 'best_model4.pth'), 19),
     load_model(os.path.join(base_dir, 'Entreno', 'best_model6.pth'), 15)
 ]
 
-# Listas de clases correspondientes a cada modelo
 class_lists = [
     os.path.join(base_dir, 'Entreno', 'best_model4.txt'),
     os.path.join(base_dir, 'Entreno', 'best_model6.txt')
 ]
 
-# Función combinada para predecir el alimento utilizando los modelos
 def predict_food_item(image_path):
     image = Image.open(image_path)
     image = data_transforms(image).unsqueeze(0)
@@ -54,6 +48,5 @@ def predict_food_item(image_path):
             classes = f.read().splitlines()
         predicted_classes.append(classes[predictions[i]])
 
-    # Votación mayoritaria entre las predicciones de los modelos
     final_prediction = max(set(predicted_classes), key=predicted_classes.count)
     return final_prediction
